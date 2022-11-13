20d0b26aa1f7c314922353d9020ee339423714f3
Junsu Lee <me@adrinerdp.co>
2022-11-13 15:14:15 +0900

    fix: 카카오 initialization 안 되는 에러 수정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import Kakao from 'kakao-js-sdk';
-import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import { ThemeProvider } from 'vue3-styled-components';
 
@@ -8,12 +6,6 @@ import BaseLayout from '@/components/layouts/BaseLayout.vue';
 import { Colors, Typography } from '@/utils/styles';
 
 const theme = { ...Colors, ...Typography };
-
-onMounted(() => {
-  if (!Kakao.isInitialized()) {
-    Kakao.init('276ca335211bcebd261cd53d447469fb');
-  }
-});
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import Kakao from 'kakao-js-sdk';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
@@ -7,5 +8,9 @@ import router from './router';
 import './utils/styles';
 
 const app = createApp(App);
+
+if (!Kakao.isInitialized()) {
+  Kakao.init('276ca335211bcebd261cd53d447469fb');
+}
 
 app.use(createPinia()).use(router).mount('#app');
